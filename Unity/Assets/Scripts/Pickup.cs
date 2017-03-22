ca697340cc9896f57e9216f7251e9ba7ff664d57
Garth Kiepper <dsdever255@gmail.com>
2017-03-21 21:45:01 -0500

    Initial commit - Unity distributable for testing and integration.

---
+++ Unity/Assets/Scripts/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    enum itemType { pellet, superPellet, fruit, blueGhost };
+    [SerializeField] itemType myType;
+    int layer_pacman = 8;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Only let the player pick this item up
+        if (other.gameObject.layer != layer_pacman)
+            return;
+
+        switch (myType)
+        {
+            case itemType.pellet:
+                break;
+            case itemType.superPellet:
+                Debug.Log("SUPER MODE ACTIVATE");
+                break;
+            case itemType.fruit:
+                break;
+            case itemType.blueGhost:
+                break;
+        }
+
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Unity/Assets/Scripts/Wakawaka.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wakawaka : MonoBehaviour
+{
+    [SerializeField] float moveSpd;
+    const int layer_level = 11;
+
+    private void FixedUpdate()
+    {
+        //Update heading
+        if (Input.GetButton("Up"))
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        else if (Input.GetButton("Down"))
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+        else if (Input.GetButton("Left"))
+            transform.rotation = Quaternion.Euler(new Vector3(0, 270, 0));
+        else if (Input.GetButton("Right"))
+            transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+
+        if (!FacingWall())
+        {
+            transform.Translate(Vector3.forward * moveSpd * Time.deltaTime);
+
+            //Animation
+
+            //Wakawaka SFX
+        }
+    }
+
+    //Raycasts right in front of PacMan to detect a wall.  Because Math, it will keep PacMan "on the grid" (center of the lanes) reasonably well
+    private bool FacingWall()
+    {
+        return Physics.Raycast(transform.position, transform.forward, 0.5f, 1 << layer_level);
+    }
+}
